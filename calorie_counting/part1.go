7c2fb50c3651d6c6f1c452e19d050715edb7c3fd
Kamran Munshi <544804+munshikamran@users.noreply.github.com>
2023-03-14 21:31:14 -0700

    Initial commit with simple solution to day1 (part 1)

---
+++ calorie_counting/part1.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	// answer := 123
+	answer := getMaximumCaloriesFromFile("input.txt")
+	fmt.Println("The Elf with the maximum number of calories has ", answer, " calories!")
+}
+
+func getMaximumCaloriesFromFile(fileName string) int {
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var totalCals float64 = 0.0
+	var maxCals float64 = 0.0
+
+	for scanner.Scan() {
+		lineStr := scanner.Text()
+		lineStr = strings.TrimSpace(lineStr)
+
+		if len(lineStr) != 0 {
+			currCals, _ := strconv.ParseFloat(lineStr, 64)
+			totalCals += currCals
+		} else {
+			maxCals = math.Max(maxCals, totalCals)
+			totalCals = 0.0
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return int(maxCals)
+}
